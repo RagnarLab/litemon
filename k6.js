a845276671625c25d6a4112d942548f7ceb70681
Arvid Gerstmann <github@arvid.io>
2025-06-06 14:36:32 +0000

    chore: k6 test script

---
+++ k6.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  iterations: 10,
+};
+
+// The default exported function is gonna be picked up by k6 as the entry point
+// for the test script. It will be executed repeatedly in "iterations" for the
+// whole duration of the test.
+export default function () {
+  // Make a GET request to the target URL
+  http.get("http://localhost:9774/metrics");
+
+  // Sleep for 1 second to simulate some real-world usage
+  sleep(1);
+}
